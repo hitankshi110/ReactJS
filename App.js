dcc3bbc4e401846cdf64593d94f393b6f541138c
hitankshi110 <hitankshishah110@gmail.com>
2023-07-26 00:02:17 +0530

    first commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is React Js.Click below to learn react.
+          React Js.Click below to learn React.
         </p>
         <a
           className="App-link"
